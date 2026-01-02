cf98e4302b43a26640bf4a7209647d6b54b8ee4b
Yusufcan Kadağan <yusufcan_kadagan@hotmail.com>
2026-01-02 20:47:37 +0300

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,5 +19,10 @@ namespace SkyCast
             InitializeComponent();
             
         }
+
+        private void InitializeComponent()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
